108166367b1a083fcaf6b97ffecb253dd8f74e4f
Zia Ulhafizd Aldiansyah <ziaulhafizda@gmail.com>
2020-08-18 15:51:54 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    <Album />
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/Header";
+import Album from "./components/Album";
+
+export default {
+  name: "App",
+  components: {
+    Album,
+    NavBar
+  }
+};
+</script>
---
+++ src/components/Album.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mt-4 container">
+    <div class="row">
+      <div v-for="photo in photos" :key="photo.id" class="col-md-4">
+        <div class="card mb-4">
+          <img class="card-img-top" :src="photo.thumbnailUrl">
+          <div class="card-body">
+            <p class="card-text">{{photo.title}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    photos: []
+  }),
+  mounted() {
+    this.getPhotos();
+  },
+  methods: {
+    async getPhotos() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/photos"
+      );
+      const json = await response.json();
+      this.photos = json;
+    }
+  }
+};
+</script>
